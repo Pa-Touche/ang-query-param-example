1d2922c7222a1378d07bf914542315a56e569080
Pa-Touche <47572440+Pa-Touche@users.noreply.github.com>
2023-07-25 22:22:19 +0200

    Added tests for both leafProperty / value methods

--- src/app/shared/utils.spec.ts
+++ src/app/shared/utils.spec.ts
@@ -1,4 +1,4 @@
-import { exctractLeafProperties } from "./utils";
+import { exctractLeafProperties, getLeafValuePairs } from "./utils";
 
 
 describe('exctractLeafProperties', () => {
@@ -8,7 +8,7 @@ describe('exctractLeafProperties', () => {
         e: {
           f: 'value1'
         },
-        g: 'value2'
+        g: 'value2',
       },
       h: 'value3'
     };
@@ -62,3 +62,82 @@ describe('exctractLeafProperties', () => {
     expect(result).toEqual(['a', 'b', 'c']);
   });
 });
+
+describe('getLeafValuePairs', () => {
+  it('should return an array of key-value pairs for leaf properties', () => {
+    const obj = {
+      a: 'value1',
+      b: {
+        c: 'value2',
+        d: {
+          e: 'value3'
+        }
+      },
+      f: [1, 2, 3],
+      g: true
+    };
+
+    const result = getLeafValuePairs(obj);
+    expect(result).toEqual([
+      ['a', 'value1'],
+      ['b.c', 'value2'],
+      ['b.d.e', 'value3'],
+      ['f', [1, 2, 3]],
+      ['g', true]
+    ]);
+  });
+
+  it('should return an empty array for an empty object', () => {
+    const obj = {};
+    const result = getLeafValuePairs(obj);
+    expect(result).toEqual([]);
+  });
+
+  it('should handle arrays as leaf properties', () => {
+    const obj = {
+      a: [1, 2, 3],
+      b: 'value',
+      c: {
+        d: [4, 5]
+      }
+    };
+
+    const result = getLeafValuePairs(obj);
+    expect(result).toEqual([
+      ['a', [1, 2, 3]],
+      ['b', 'value'],
+      ['c.d', [4, 5]]
+    ]);
+  });
+
+  it('should handle deeply nested objects', () => {
+    const obj = {
+      a: {
+        b: {
+          c: {
+            d: 'value'
+          }
+        }
+      }
+    };
+
+    const result = getLeafValuePairs(obj);
+    expect(result).toEqual([['a.b.c.d', 'value']]);
+  });
+
+  it('should handle an object with multiple leaf properties', () => {
+    const obj = {
+      a: 'value1',
+      b: 'value2',
+      c: 'value3'
+    };
+
+    const result = getLeafValuePairs(obj);
+    expect(result).toEqual([
+      ['a', 'value1'],
+      ['b', 'value2'],
+      ['c', 'value3']
+    ]);
+  });
+});
+
